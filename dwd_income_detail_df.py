1dfdb1bdc640472b1d6ff02c20a6b065cc410d35
yeomanyang <yeomanyang@tencent.com>
2022-02-13 12:23:19 +0800

    修改
    修改后缀

---
+++ dwd_income_detail_df.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+time = __import__('time')
+string = __import__('string')
+from datetime import *
+
+def function():
+	pass
---
+++ dwd_income_df.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+time = __import__('time')
+string = __import__('string')
+from datetime import *
