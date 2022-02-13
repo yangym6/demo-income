6834002984cde7204f689648aa842364f2fd0b20
yeomanyang <yeomanyang@tencent.com>
2022-02-13 12:24:47 +0800

    个漏了
    个漏了

--- dwd_income_detail_di.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
-
-time = __import__('time')
-string = __import__('string')
-from datetime import *
-
-def function():
-	pass
--- dwd_income_di.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
-
-time = __import__('time')
-string = __import__('string')
-from datetime import *
